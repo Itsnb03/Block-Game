bf961bc3c3338b8afcc6dd3b6944dfcae5b46519
Itsnb03 <132834489+Itsnb03@users.noreply.github.com>
2024-01-30 21:18:35 +0530

    Drawing a window for the game
    Drawing a window for the game

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+
+# Set up the drawing window
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
